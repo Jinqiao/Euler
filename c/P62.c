eb7b623eb5f4256996a2f7f3a1bd98d5a129c45e
jq <lijinqiao222@gmail.com>
2019-04-04 21:14:36 +0000

    working on P62.c

---
+++ c/P62.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <glib.h>
+
+int comp(const void *a,const void *b) {
+  char *x = (char *) a;
+  char *y = (char *) b;
+  return *x - *y;
+}
+
+int main(int argc, char *argv[])
+{
+  long i = 0, c = 0;
+  char k[30];
+  GHashTable *hash = g_hash_table_new(g_str_hash, g_str_equal);
+  GSList *list = NULL;
+
+  while (1) {
+    i++;
+    c = i * i * i;
+    sprintf(k, "%ld", c);
+    qsort(k, strlen(k), sizeof(char), comp);
+
+    list = g_hash_table_lookup(hash, k);
+    if(list == NULL){
+      g_hash_table_insert(hash, k, g_slist_prepend(NULL, GINT_TO_POINTER(c)));
+    }
+    else{
+      list = g_slist_prepend(list, GINT_TO_POINTER(c));
+    }
+
+    if(i == 5027){
+      printf("k: %s, c: %ld, l: %d\n", k, c, g_slist_length(list));
+    }
+
+    if (g_slist_length(list) >= 3 || i > 5030) {
+      break;
+    }
+  }
+
+  GSList *iter = NULL;
+  for (iter = list; iter; iter = iter -> next) {
+    printf("k: %s, %ld\n", k, (long)iter -> data);
+  }
+
+  return 0;
+}
